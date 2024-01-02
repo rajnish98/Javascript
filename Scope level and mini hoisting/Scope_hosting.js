53ce48d21d4dc2517c4f41d9dd0d9af468633975
rajnish98 <rajnishpandey63@gmail.com>
2024-01-02 23:16:05 +0530

    scope and hoisting

---
+++ Scope level and mini hoisting/Scope_hosting.js
@@ -0,0 +1,27 @@
+//Scope in javascript
+// function one(){
+//   const username = "Rajnish"
+//   function two(){
+//     const website = "youtube"
+//     console.log(username);
+//   }
+  // console.log(website);
+//   two();
+// }
+// one();
+
+if (true) {
+  const username = "Rajnish"
+  if (username === "rajnish") {
+    const website = " youtube"
+    console.log(username + website);
+  }
+  // console.log(website);
+}
+// console.log(username);
+
+// hoisting
+var1(5)
+const var1 = function (num) {
+  return num + 1;
+}
