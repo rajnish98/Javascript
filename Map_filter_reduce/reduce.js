ef692bf4232774e474d1b5747feb842d2bccb87b
rajnish98 <rajnishpandey63@gmail.com>
2024-02-21 22:08:57 +0530

    reduce

---
+++ Map_filter_reduce/reduce.js
@@ -0,0 +1,35 @@
+const arr = [5, 1, 3, 2, 6];
+
+// normal method to find sum
+function findSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
+}
+
+console.log(findSum(arr));
+
+
+// reduce method to find sum
+
+const output = arr.reduce(function(acc,curr){
+  acc += curr;
+  return acc;
+}, 0);
+console.log(output);
+
+// sum pof max normal method
+
+function findMax(arr){
+  let max = 0;
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i] > max){
+      max = arr[i];
+    }
+   }
+   return max;
+}
+
+console.log(findMax(arr));
