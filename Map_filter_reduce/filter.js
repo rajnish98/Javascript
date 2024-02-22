646f682d03af12bd017ac2edea6aa544c2038496
rajnish98 <rajnishpandey63@gmail.com>
2024-02-22 20:29:33 +0530

    filter trickey exaple

--- Map_filter_reduce/filter.js
+++ Map_filter_reduce/filter.js
@@ -6,3 +6,15 @@ const arr = [5, 1, 3, 2, 6];
 
  const output = arr.filter(isEven);
  console.log(output);
+
+
+ const user = [
+  {firstName: "rajnish", lastName: "pandey", age: 28},
+  {firstName: "raj", lastName: "pandey", age: 27},
+  {firstName: "raj babu", lastName: "pandey", age: 26},
+  {firstName: "rajnikant", lastName: "pandey", age: 25},
+];
+
+const output1 = user.filter((x)=> x.age < 30) .map((x) => x.firstName);
+
+console.log(output1);
