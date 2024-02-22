2e2a070af2351cc0316a61ba7a23c4f149a2e00c
rajnish98 <rajnishpandey63@gmail.com>
2024-02-22 20:01:21 +0530

    trickey map example

--- Map_filter_reduce/map.js
+++ Map_filter_reduce/map.js
@@ -7,3 +7,15 @@ console.log(output);
 
 
 // tricky map
+
+
+const user = [
+  {firstName: "rajnish", lastName: "pandey", age: 28},
+  {firstName: "raj", lastName: "pandey", age: 27},
+  {firstName: "raj babu", lastName: "pandey", age: 26},
+  {firstName: "rajnikant", lastName: "pandey", age: 25},
+];
+
+const output1 = user.map(x => x.firstName + " " + x.lastName);
+
+console.log(output1);
